aac09a7f1a14939f058ff795f6fba52f42fd1b26
vokalke2 <121307561+vokalke2@users.noreply.github.com>
2023-09-25 16:50:38 -1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+//identitas saya
+let nama = "Muhamad Rizki Akbar";
+let nim = "20220040166";
+
+//operasi aritmatika
+let angka1 = 10;
+let angka2 = 5;
+let tambah = angka1 + angka2;
+let kurang = angka1 - angka2;
+let kali = angka1 * angka2;
+let bagi = angka1 / angka2;
+
+//menampilkan identitas dan operasi aritmatika
+console.log("Nama:" + nama);
+console.log("NIM:" + nim);
+console.log("Hasil Penjumlahan:"+ tambah);
+console.log("Hasil Pengurangan:"+ kurang);
+console.log("Hasil Perkalian:" + kali);
+console.log("Hasil Pembagian:"+ bagi);
