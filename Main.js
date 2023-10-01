64d21ffbb84484dfa4e056557e330f86e7f52809
vokalke2 <121307561+vokalke2@users.noreply.github.com>
2023-09-30 18:32:04 -1000

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,8 @@
+var firstName = "Rizki";
+var lastName = "Akbar"; 
+var age = 19; 
+var isMarried = false;
+
+console.log("Nama saya",firstName+" "+lastName);
+console.log("Usia saya",age);
+console.log("hasil isMarried",isMarried);
